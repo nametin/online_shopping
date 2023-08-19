371a1bff06029d40eb484036ae40d415bb64c3fc
nametin <102544163+nametin@users.noreply.github.com>
2023-08-20 01:49:49 +0300

    ürün sayfası eklendi

--- product/models.py
+++ product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    product_id = models.AutoField
+    id = models.AutoField
     product_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
